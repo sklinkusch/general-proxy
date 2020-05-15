e333201909b9d591d244c5e2795d54ecc7d3007f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-15 11:51:22 +0200

    fix error treatment

--- errors.js
+++ errors.js
@@ -1,10 +1,8 @@
 exports.wrongEndpoint = (req, res) => {
-  return res
-    .status(404)
-    .json({
-      error: {
-        message:
-          "The endpoint you selected does not exist. Please refer the the manual.",
-      },
-    })
+  return res.status(404).json({
+    error: {
+      message:
+        "The endpoint you selected does not exist. Please refer the the manual.",
+    },
+  })
 }
