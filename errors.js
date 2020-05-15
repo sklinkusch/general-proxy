23ce49cd6065f7e8657d08f85770b87e6911fb5b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-15 15:06:02 +0200

    separated checks to make it more understandable

--- errors.js
+++ errors.js
@@ -1,29 +1,34 @@
 exports.serverInRequest = (req, res, next) => {
   if ("server" in req.query) {
-    const { server } = req.query
-    if (typeof server == "string") {
-      if (server.startsWith("http://") || server.startsWith("https://")) {
-        next()
-      } else {
-        return res
-          .status(400)
-          .json({
-            error: {
-              message: "The server has to start with 'http://' or 'https://'.",
-            },
-          })
-      }
-    } else {
-      return res
-        .status(400)
-        .json({ error: { message: "The server has to be a string." } })
-    }
+    next()
+  } else {
+    return res.status(400).json({
+      error: { message: "You have to provide a server in your request." },
+    })
+  }
+}
+
+exports.wrongArgType = (req, res, next) => {
+  const { server } = req.query
+  if (typeof server == "string") {
+    next()
   } else {
     return res
       .status(400)
-      .json({
-        error: { message: "You have to provide a server in your request." },
-      })
+      .json({ error: { message: "The server has to be a string." } })
+  }
+}
+
+exports.checkAddress = (req, res, next) => {
+  const { server } = req.query
+  if (server.startsWith("http://") || server.startsWith("https://")) {
+    next()
+  } else {
+    return res.status(400).json({
+      error: {
+        message: "The server has to start with 'http://' or 'https://'.",
+      },
+    })
   }
 }
 
--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const { postData } = require("./requests/post")
 const { putData } = require("./requests/put")
 const { patchData } = require("./requests/patch")
 const { deleteData } = require("./requests/delete")
-const { wrongEndpoint, serverInRequest } = require("./errors")
+const {
+  wrongEndpoint,
+  serverInRequest,
+  wrongArgType,
+  checkAddress,
+} = require("./errors")
 
 app.use("/", (req, res, next) => serverInRequest(req, res, next))
+app.use("/", (req, res, next) => wrongArgType(req, res, next))
+app.use("/", (req, res, next) => checkAddress(req, res, next))
 app.get("/", (req, res) => getData(req, res))
 app.post("/", (req, res) => postData(req, res))
 app.put("/", (req, res) => putData(req, res))
