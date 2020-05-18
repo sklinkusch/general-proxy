abe16d9a55599383be8a49c2e579492e7e9e391b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-18 11:07:03 +0200

    allow headers&body also on get and delete requests

--- requests/delete.js
+++ requests/delete.js
@@ -3,11 +3,12 @@ const axios = require("axios")
 exports.deleteData = async (req, res) => {
   try {
     const { server } = req.query
+    const { headers, body } = req
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
-    const response = await axios.delete(server)
-    const { headers, data } = await response
-    return res.status(200).json(data)
+    const response = await axios.delete(server, { headers, body })
+    const { headers: responseHeaders, data: responseData } = await response
+    return res.status(200).json(responseData)
   } catch (error) {
     return res.status(500).json(error)
   }
--- requests/get.js
+++ requests/get.js
@@ -3,11 +3,12 @@ const axios = require("axios")
 exports.getData = async (req, res) => {
   try {
     const { server } = req.query
+    const { headers, body } = req
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
-    const response = await axios.get(server)
-    const { headers, data } = await response
-    return res.status(200).json(data)
+    const response = await axios.get(server, { headers, body })
+    const { headers: responseHeaders, data: responseData } = await response
+    return res.status(200).json(responseData)
   } catch (error) {
     return res.status(500).json(error)
   }
