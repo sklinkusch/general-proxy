f6e2b5dc8f99019593c62d80481cf8a71a5f8692
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-18 11:33:55 +0200

    add logs on get requests

--- requests/get.js
+++ requests/get.js
@@ -3,11 +3,16 @@ const axios = require("axios")
 exports.getData = async (req, res) => {
   try {
     const { server } = req.query
+    console.log(server)
     const { headers } = req
+    console.log(headers)
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
     const response = await axios.get(server, { headers })
+    console.log(response)
     const { headers: responseHeaders, data: responseData } = await response
+    console.log(responseHeaders)
+    console.log(responseData)
     return res.status(200).json(responseData)
   } catch (error) {
     return res.status(500).json(error)
