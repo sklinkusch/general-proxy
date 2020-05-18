ced88df985fbed72681acb78fcae511a823d0356
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-18 09:19:05 +0200

    logging everything for get/post requests

--- requests/get.js
+++ requests/get.js
@@ -2,13 +2,18 @@ const axios = require("axios")
 
 exports.getData = async (req, res) => {
   try {
+    console.log("entering routine for get requests")
     const { server } = req.query
+    console.log(`server is set to ${server}`)
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
+    console.log("set headers")
     const response = await axios.get(server)
     const { headers, data } = await response
+    console.log("successful request")
     return res.status(200).json(data)
   } catch (error) {
+    console.log("unsuccessful request")
     return res.status(500).json(error)
   }
 }
--- requests/post.js
+++ requests/post.js
@@ -2,14 +2,19 @@ const axios = require("axios")
 
 exports.postData = async (req, res) => {
   try {
+    console.log("entering routine for post requests")
     const { server } = req.query
+    console.log(`server is set to ${server}`)
     const { headers, data } = req
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
+    console.log("headers are set")
     const response = await axios.post({ url: server, headers, data })
     const { headers: responseHeaders, data: responseData } = await response
+    console.log("successful request")
     return res.status(200).json(responseData)
   } catch (error) {
+    console.log("unsuccessful request")
     return res.status(500).json(error)
   }
 }
